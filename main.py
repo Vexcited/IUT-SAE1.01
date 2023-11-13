176727f50cc520f88946525bf994179204ed52ca
Mikkel RINGAUD <mikkel@milescode.dev>
2023-11-13 09:49:47 +0100

    feat(main): add Q to quit game

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from utils.centrer import centrer
 from utils.effacer_ecran import effacer_ecran
 
 if __name__ == "__main__":
-  effacer_ecran()
+    sélection: str
+    sélection = ""
 
-  print(centrer("Bienvenue!"))
-  print("")
-  print(centrer("1: Devinette"))
-  print(centrer("2: Allumettes"))
-  print(centrer("3: Morpion"))
-  print("")
-  print(centrer("Q: Sortir"))
-  print("")
-  input("Séléction(1,2,3,Q) : ")
+    while (sélection.upper() != "Q"):
+        effacer_ecran()
+
+        print(centrer("Bienvenue, dans la sélection de mini-jeu !"))
+        print("")
+
+        print(centrer("1: Devinette"))
+        print(centrer("2: Allumettes"))
+        print(centrer("3: Morpion"))
+        print("")
+
+        print(centrer("Q: Sortir"))
+        print("")
+
+        sélection = input("Sélection(1,2,3,Q) : ")
