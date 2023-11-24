f2ba9a9cf7afecd0a51e88e7fcbfb1014ae81a77
Mikkel RINGAUD <mikkel@milescode.dev>
2023-11-24 13:36:02 +0100

    fix(devinette): add game score + new player names

--- jeux/devinette.py
+++ jeux/devinette.py
@@ -1,6 +1,5 @@
 from getpass import getpass
-from utils.afficher_tour import afficher_tour
-from joueurs.afficher import nom_joueur
+from utils.titre import séparateur_avec_titre
 
 def calcul_score(nombre_tour : int) -> int:
     """
@@ -10,14 +9,11 @@ def calcul_score(nombre_tour : int) -> int:
 
     score : int
 
-    # Vu que l'on peut avoir un nombre de tour de 1 à 100,
-    # on peut calculer le score en soustrayant le nombre de
-    # tour au nombre 100.
-    score = 100 - nombre_tour
+    score = int(100 * (1 / nombre_tour))
     return score
 
 
-def main_devinette() -> None:
+def main_devinette(joueur1: str, joueur2: str) -> None:
     """
     Procédure de point d'entrée pour le jeu de devinette.
     """
@@ -29,45 +25,44 @@ def main_devinette() -> None:
     max : int
     nombre_tour : int
     jeu_en_cours : bool
+    triche: bool
 
     min = 1
     max = 100 # On défini la limite du nombre mystère
     nombre_tour = 1
     jeu_en_cours = True
+    triche = False
 
     # On demande au joueur 1 de sélectionner le nombre mystère
     # que le joueur 2 devra trouver entre 1 et 100
-    nombre_mystère = int(getpass(nom_joueur(1) + ", choisissez le nombre mystère (entre 1 et 100) que " + nom_joueur(2) + " devra trouver: "))
+    nombre_mystère = int(getpass(joueur1 + ", choisissez le nombre mystère (entre 1 et 100) que " + joueur2 + " devra trouver: "))
 
     while jeu_en_cours:
-        afficher_tour(nombre_tour)
+        print(séparateur_avec_titre("Tour " + str(nombre_tour)))
 
         # Proposition
-        proposition = int(input("\n" + nom_joueur(2) + ", veuillez choisir un nombre entre " + str(min) + " et " + str(max) + " : "))
+        proposition = int(input("\n" + joueur2 + ", veuillez choisir un nombre entre " + str(min) + " et " + str(max) + " : "))
         # Si on donne une proposition en dehors des limites, on redemande une proposition.
         while not proposition <= max and proposition >= min:
-            proposition = int(input("\n" + nom_joueur(2) + ", votre proposition dois être entre " + str(min) + " et " + str(max) + " : "))
+            proposition = int(input("\n" + joueur2 + ", votre proposition dois être entre " + str(min) + " et " + str(max) + " : "))
         
         # On utilise .strip pour enlever les espaces avant et après la réponse.
-        réponse = input("\n" + nom_joueur(1) + ", votre réponse (répondez par 'trop petit', 'trop grand' ou 'c'est gagné') : ").strip().lower()
+        réponse = input("\n" + joueur1 + ", votre réponse (répondez par 'trop petit', 'trop grand' ou 'c'est gagné') : ").strip().lower()
         
         # ANTI-CHEAT
         if réponse == "trop petit" and not proposition < nombre_mystère :
-            print(nom_joueur(1) + ", vous avez triché !")
-            #terminer le jeu et ajouter les scores au joueur 2
+            triche = True
             jeu_en_cours = False
 
         elif réponse == "trop grand" and not proposition > nombre_mystère :
-            print(nom_joueur(1) + ", vous avez triché !")
-            #terminer le jeu et ajouter les scores au joueur 2
+            triche = True
             jeu_en_cours = False
 
         elif réponse == "c'est gagné" and not proposition == nombre_mystère :
-            print(nom_joueur(1) + ", vous avez triché !")
-            #terminer le jeu et ajouter les scores au joueur 2
+            triche = True
             jeu_en_cours = False
 
-        else :
+        else:
             if réponse == "trop petit":
                 min = proposition + 1
             if réponse == "trop grand":
@@ -77,7 +72,13 @@ def main_devinette() -> None:
                 jeu_en_cours = False
         
         nombre_tour += 1
-                
+
+    if (triche):
+        print("\n" + joueur1 + ", vous avez triché !")
+        print(joueur2 + ", vous avez gagné mais aucun point ne vous est attribué.")
+    else:
+        print("\n" + joueur2 + ", vous avez trouvé le nombre mystère en " + str(nombre_tour) + " tours !")
+        print("Votre score est de " + str(calcul_score(nombre_tour)) + " points !") 
 
 if __name__ == "__main__":
-    main_devinette()
+    main_devinette("Joueur1", "Joueur2")
