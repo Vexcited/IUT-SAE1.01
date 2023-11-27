677806abdcef02671892476107b2675af3f66c5a
Mikkel RINGAUD <mikkel@milescode.dev>
2023-11-27 09:51:23 +0100

    fix(p4): center correctly when coloring

--- p4.py
+++ p4.py
@@ -1,30 +1,44 @@
 from utils.afficher_tour import afficher_tour
 from utils.centrer import centrer
+from os import get_terminal_size
 
 def afficher_p4(jeu:list[list[str]])->None:
     """
     Procédure qui permet d'afficher proprement le jeu
-        entrée : le jeun un tableau de tableau de chaine de caractères
+        entrée : le jeu dans un tableau de tableau de chaîne de caractères
     """
     i : int
     j : int
     ligne : str
+    # On crée une chaîne SANS les couleurs pour calculer la taille de la ligne.
+    raw_ligne : str
+    # `pad` et `lpad` sont les espacements à gauche pour centrer le jeu en fonction de `raw_ligne`.
+    pad : int
+    lpad : int
+    # On récupère la taille du terminal pour centrer le jeu.
+    terminal_colonne: int
+    terminal_colonne = get_terminal_size().columns
 
-    ligne = ""
     print(centrer("\u256D\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u254C\u256E"))
     for i in range(0, 6):
-        ligne = ""
-        ligne += "\u2502 "
+        ligne = "\u2502 "
+        raw_ligne = "\u2502 "
 
         for j in range(0, 7):
             ligne += jeu[i][j] + " "
+            raw_ligne += "0 "
 
         ligne += "\u2502 "
-        print(centrer(ligne))
+        raw_ligne += "\u2502 "
+
+        # On calcule manuellement l'espacement pour centrer le jeu
+        pad = max(0, terminal_colonne - len(raw_ligne))
+        lpad = (pad + 1) // 2
+        print(lpad * " " + ligne) 
     print(centrer("\u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518"))
 
 def main_p4(joueur1 : str, joueur2 : str)->None:
-    """Procedure de point d'entrée pour le jeu de puissance 4"""
+    """Procédure de point d'entrée pour le jeu de puissance 4"""
 
     jeu : list[list[str]]
     en_cours : bool
@@ -152,8 +166,9 @@ def main_p4(joueur1 : str, joueur2 : str)->None:
                 if jeu[i][j] == jeu[i+1][j] and jeu[i+2][j] == jeu[i+3][j] and jeu[i][j] == jeu[i+3][j] and jeu[i][j] != ".":
                     # le joueur qui viens de jouer a gagné
                     # ajout des scores
-                    print(joueur1, "a gagné")
+                    print(joueur2, "a gagné")
                     en_cours = False
+                    afficher_p4(jeu)
 
         ## Test colonne gagnante
         for i in range(0, 6):
@@ -161,8 +176,9 @@ def main_p4(joueur1 : str, joueur2 : str)->None:
                 if jeu[i][j] == jeu[i][j+1] and jeu[i][j+2] == jeu[i][j+3] and jeu[i][j] == jeu[i][j+3] and jeu[i][j] != ".":
                     # le joueur qui viens de jouer a gagné
                     # ajout des scores
-                    print(joueur1, "a gagné")
+                    print(joueur2, "a gagné")
                     en_cours = False
+                    afficher_p4(jeu)
 
         ## Test diagonales gagnantes
         ### diagonale : haut de droite -> bas a gauche
@@ -171,8 +187,9 @@ def main_p4(joueur1 : str, joueur2 : str)->None:
                 if jeu[i][j] == jeu[i+1][j+1] and jeu[i+2][j+2] == jeu[i+3][j+3] and jeu[i][j] == jeu[i+3][j+3] and jeu[i][j] != ".":
                     # le joueur qui viens de jouer a gagné
                     # ajout des scores
-                    print(joueur1, "a gagné")
+                    print(joueur2, "a gagné")
                     en_cours = False
+                    afficher_p4(jeu)
 
         ### diagonale : haut a gauche -> bas a droite
         for i in range(0, 3):
@@ -180,8 +197,9 @@ def main_p4(joueur1 : str, joueur2 : str)->None:
                 if jeu[i+3][j] == jeu[i+2][j+1] and jeu[i+1][j+2] == jeu[i][j+3] and jeu[i+3][j] == jeu[i][j+3] and jeu[i+3][j] != ".":
                     # le joueur qui viens de jouer a gagné
                     # ajout des scores
-                    print(joueur1, "a gagné")
+                    print(joueur2, "a gagné")
                     en_cours = False
+                    afficher_p4(jeu)
 
         nb_tour += 1
 
